63eed12e280909638708dcac36eac9afae34c2bb
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-09-13 11:26:30 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,33 @@
 package lv.rvt;
+import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
         // Make your Hello World programm!
+
+        // System.out.println("Hello Java!"); System.out.println("Hello Java!");
+        // System.out.println("Hello Java!"); System.out.println("Hello Java!");
+      
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(". . .");
+        String name = scanner.nextLine();
+
+        System.out.println(". . .");
+        String lastname = scanner.nextLine();
+
+        System.out.println(". . .");
+        String group = scanner.nextLine();
+  
+        System.out.println(". . .");
+
+
+        System.out.println("Students: " + " " +  name  + " " + lastname +" " + group);
+
+
+
+    
     }
 }
