295e9e3c85e7ea4bc74843606b374aca5b3946ea
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-09-27 11:39:14 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,40 +11,29 @@ public class App
 
     // mana programma
 
-     System.out.println("Give points [0-100]:");
-     int box = Integer.valueOf(scanner.nextLine());
+        System.out.print("Value of the gift? ");
+        int box = Integer.valueOf(scanner.nextLine());
 
-     if (box <= 49) {
-        System.out.println("Grade: failed");
-     }
+        double tax;
 
-     else if (box <= 59) {
-        System.out.println("Grade: 1");
-     }
-
-     else if (box <= 69) {
-        System.out.println("Grade: 2");
-     }
-
-     else if (box <= 79) {
-        System.out.println("Grade: 3");
-     }
-     else if (box <= 89) {
-        System.out.println("Grade: 4");
-     }
-
-     else if (box <= 100) {
-        System.out.println("Grade: 5");
-     }
-
-     else if (box > 100) {
-        System.out.println("incredible!");
-     }
-
-     else if (box < 0) {
-        System.out.println("impossible!");
-     }
-
-
+    if (box < 5000) {
+        System.out.println("No tax!");
+    } else if (box <= 25000) {
+        tax = 100 + (box - 5000) * 0.08;
+        System.out.printf("Tax: %.1f%n", tax);
+    } else if (box <= 55000) {
+        tax = 1700 + (box - 25000) * 0.10;
+        System.out.printf("Tax: %.1f%n", tax);
+    } else if (box <= 200000) {
+        tax = 4700 + (box - 55000) * 0.12;
+        System.out.printf("Tax: %.1f%n", tax);
+    } else if (box <= 1000000) {
+        tax = 22100 + (box - 200000) * 0.15;
+        System.out.printf("Tax: %.1f%n", tax);
+    } else {
+        tax = 142100 + (box - 1000000) * 0.17;
+        System.out.printf("Tax: %.1f%n", tax);
     }
+
+}
 }
