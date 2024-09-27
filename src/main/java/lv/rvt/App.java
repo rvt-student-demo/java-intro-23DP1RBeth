335fd21d5628a6d9e3a25852fa4dcc0a213e4acc
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-09-27 11:05:04 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,45 +11,31 @@ public class App
 
     // mana programma
 
-     int number = 49;
-     int number_2 = 59;
-     int number_3 = 69;
-     int number_4 = 79;
-     int number_5 = 89;
-     int number_6 = 100;
-     int number_7 = 110;
-
      System.out.println("uzraksti cik tev: ");
      int box = Integer.valueOf(scanner.nextLine());
 
-     if (box < number) {
+     if (box <= 49) {
         System.out.println("Grade: failed");
      }
 
-     else if (box < number_2) {
+     else if (box <= 59) {
         System.out.println("Grade: 1");
      }
 
-     else if (box < number_3) {
+     else if (box <= 69) {
         System.out.println("Grade: 2");
      }
 
-     else if (box < number_4) {
+     else if (box <= 79) {
         System.out.println("Grade: 3");
      }
-     else if (box < number_5) {
+     else if (box <= 89) {
         System.out.println("Grade: 4");
      }
 
-     else if (box < number_6) {
-
+     else if (box <= 100) {
         System.out.println("Grade: 5");
      }
 
-     else if (box < number_7) {
-
-        System.out.println("impossible!");
-
-     }
     }
 }
