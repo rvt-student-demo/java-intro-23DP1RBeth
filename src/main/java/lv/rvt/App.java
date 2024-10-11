f6d004282006e2017f0693f6eff48e1689f40d09
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-10-11 10:57:44 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,56 +1,23 @@
 package lv.rvt;
-import java.util.*;
+import java.util.Scanner;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        // int value = Integer.valueOf(scanner.nextLine());
-
+public class App {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give numbers: ");
-        
-        int sum = 0;
+        System.out.println("Cik reizes?");
+        int times = Integer.parseInt(scanner.nextLine());
 
-        int kop = 0;
+        int count = 0;
+        while (count < times) {
+            printText();
+            count++;
+        }   
+        scanner.close();
+    }
 
-
-        while (true) {
-            
-            int com = Integer.valueOf(scanner.nextLine());
-
-            if (com == -1) {
-
-                float average = sum / kop;
-
-                System.out.println("Thx! Bye!");
-
-                System.out.println("Sum: " + sum);
-
-                System.out.println("Numbers: " + kop);
-
-                System.out.println("Average:" + average);   
-
-                if (kop % 2 == 0) {
-                    System.out.println("Even: " + kop);
-                  } else {
-                    System.out.println("Odd: " + kop);
-                  }
-
-                break;
-   
-            }
-
-            if(com >= 0) {
-
-                sum += com;
-
-                kop++;
-            }
-
-        }
-    }}
-
-        
-
+    public static void printText() {
+       
+        System.out.println("Dziļumā zem zemes dzīvoja metode");
+    }
+}
