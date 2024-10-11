6464f33fd33f6817cd693e28554225963b8f2df2
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-10-11 10:03:17 +0000

    Average of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -13,21 +13,37 @@ public class App
         
         int sum = 0;
 
-    
+        int kop = 0;
+
+
         while (true) {
             
-
             int com = Integer.valueOf(scanner.nextLine());
 
-            sum += com;
-    
             if (com == -1) {
-    
+
+                float average = sum / kop;
+
                 System.out.println("Thx! Bye!");
 
-                System.out.println(sum);
+                System.out.println("Sum: " + sum);
 
-        }
+                System.out.println("Numbers: " + kop);
+
+                System.out.println("Average:" + average);   
+
+                break;
+
+            }
+
+            if(com >= 0) {
+
+                sum += com;
+
+                kop++;
+
+            }
+
         }
     }}
 
