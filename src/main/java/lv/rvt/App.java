51f5d673ffe5308e4d9d6ee8909cc3aed0c3129b
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-10-25 10:26:49 +0000

    Christmas tree

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,23 +1,37 @@
 package lv.rvt;
-import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Cik reizes?");
-        int times = Integer.parseInt(scanner.nextLine());
-
-        int count = 0;
-        while (count < times) {
-            printText();
-            count++;
-        }   
-        scanner.close();
+        public static void printPattern() {
+            
+            int n = 10; 
+    
+            for (int i = 0; i < n; i++) {
+                
+                for (int j = 0; j < n - i - 1; j++) {
+                    System.out.print(" ");
+                }
+              
+                for (int k = 0; k < 2 * i + 1; k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+    
+            for (int i = 0; i < 2; i++) { 
+           
+                System.out.print("        ");
+             
+                for (int j = 0; j < 3; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    
+        public static void main(String[] args) {
+            printPattern();
+        }
     }
-
-    public static void printText() {
-       
-        System.out.println("Dziļumā zem zemes dzīvoja metode");
-    }
-}
+    
+    
