5f79cd6878b5a81b4ab197729cd25157ee282ea5
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-10-31 12:57:55 +0000

    Same Sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,21 +2,27 @@ package lv.rvt;
 
 public class App {
 
-    class Exercise3
+    class Exercise4
     {
       public static void main ( String[] args )
       {
-        int[] valA = { 13, -22, 82, 17 }; 
-        int[] valB = { -12, 24, -79, -13 };
-        int[] sum  = { 0, 0, 0, 0 };
+        int[] valA   = { 13, -22,  82,  17}; 
+        int[] valB   = {  0,   0,   0,   0};
+
+        valB[0] = 25 - valA[0];
+        valB[1] = 25 - valA[1];
+        valB[2] = 25 - valA[2];
+        valB[3] = 25 - valA[3];
+
+        System.out.println( "valA: " 
+            + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
      
-        sum[0] = valA[0] + valB[0];
-        sum[1] = valA[1] + valB[1];
-        sum[2] = valA[2] + valB[2];
-        sum[3] = valA[3] + valB[3];
+        System.out.println( "valB: " 
+            + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
     
-        System.out.println("sum: " 
-            + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3]);
+        System.out.println( "sum:  " 
+            + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
+            + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
        }
     }
     
