c632c086efc9a0c03f28aed3bea365568f6ba324
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-10-31 12:31:39 +0000

    Array Sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,36 +2,21 @@ package lv.rvt;
 
 public class App {
 
-        public static void printPattern() {
-            
-            int n = 10; 
+    class Exercise1
+    {
+      public static void main ( String[] args )
+      {
+        int[] val = {0, 1, 2, 3}; 
+        int sum = 0;
     
-            for (int i = 0; i < n; i++) {
-                
-                for (int j = 0; j < n - i - 1; j++) {
-                    System.out.print(" ");
-                }
-              
-                for (int k = 0; k < 2 * i + 1; k++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
-    
-            for (int i = 0; i < 2; i++) { 
-           
-                System.out.print("        ");
-             
-                for (int j = 0; j < 3; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
+        for (int i = 0; i < val.length; i++) {
+          sum += val[i];
         }
     
-        public static void main(String[] args) {
-            printPattern();
-        }
+        System.out.println( "Sum of all numbers = " + sum );
+       }
+    }
+    
     }
     
     
