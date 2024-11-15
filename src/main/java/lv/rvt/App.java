92e9ac9c57bfbd8407dbbf46278c928d53710fa4
23DP1RBeth <166509379+23DP1RBeth@users.noreply.github.com>
2024-11-15 12:07:52 +0000

    The class template

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,34 +1,9 @@
 package lv.rvt;
 
-import java.util.Scanner;
-
 public class App {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Statistics allNumbers = new Statistics();
-        Statistics evenNumbers = new Statistics();
-        Statistics oddNumbers = new Statistics();
-
-        System.out.println("Enter numbers:");
-
-        while (true) {
-            int number = scanner.nextInt();
-            if (number == -1) {
-                break; 
-            }
-            allNumbers.addNumber(number); 
-            
-            if (number % 2 == 0) {
-                evenNumbers.addNumber(number); 
-            } else {
-                oddNumbers.addNumber(number); 
-            }
-        }
-
-        System.out.println("Sum: " + allNumbers.sum());
-        System.out.println("Sum of even numbers: " + evenNumbers.sum());
-        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
-
-        scanner.close();
-    }
+  public static void main(String[] args) {
+      PaymentCard card = new PaymentCard(50);
+      System.out.println(card);
+  }
 }
+
